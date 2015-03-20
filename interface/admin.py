ca4c110061f6c788a3f9e95926d57224cf4cdf07
scorpionhiccup <sharvil.katariya@students.iiit.ac.in>
2015-03-21 02:16:12 +0530

    Adding Tasks to Model Admin

--- interface/admin.py
+++ interface/admin.py
@@ -21,8 +21,32 @@
 #
 
 from django.contrib import admin
-from .models import Proxy
+from .models import Proxy, Task
 
 # Register your models here.
 
+def add_broken_url(modelAdmin, request, queryset):
+    queryset.update(broken_url = True)
+add_broken_url.short_description = 'Add Broken Url tag'
+
+def remove_broken_url(modelAdmin, request, queryset):
+    queryset.update(broken_url = False)
+remove_broken_url.short_description = 'Remove Broken Url tag'
+
+def enable_javaplugin(modelAdmin, request, queryset):
+    queryset.update(no_javaplugin = True)
+add_broken_url.short_description = 'Enable Java Plugins'
+
+def disable_javaplugin(modelAdmin, request, queryset):
+    queryset.update(no_javaplugin = False)
+disable_javaplugin.short_description = 'Disable Java Plugins'
+
+class TaskAdmin(admin.ModelAdmin):
+    #list_display = ['__unicode__', 'proxy', 'broken_url']
+    list_display = ['proxy', 'broken_url', 'no_javaplugin']
+    date_hierarchy = 'submitted_on'
+    actions = [add_broken_url, remove_broken_url, 
+        enable_javaplugin, disable_javaplugin]
+
 admin.site.register(Proxy)
+admin.site.register(Task, TaskAdmin)
