b0191db46446129b9fa39fba6ba710f7e634748d
Tarun Kumar <reachtarunhere@gmail.com>
2015-08-02 10:40:02 +0000

    Removed uneccesary loads call.

--- interface/management/commands/fdaemon.py
+++ interface/management/commands/fdaemon.py
@@ -164,14 +164,14 @@ class Command(BaseCommand):
     def get_backend_status(self,pending_id_list):
     	if not pending_id_list:
     	    return []
-        semicolon_seperated = ";".join(pending_id_list) + ";"
+        semicolon_seperated = ";".join(pending_id_list)
         status_headers = {'Authorization': 'ApiKey {}:{}'.format(API_USER,API_KEY)}
         status_url = BACKEND_HOST + "/api/v1/status/set/{}/?format=json".format(semicolon_seperated)
         try:
             r = requests.get(status_url, headers=status_headers)
         except requests.exceptions.ConnectionError:
             log.debug("Got a requests.exceptions.ConnectionError exception, will try again later.")
-        response = loads(r.json())
+        response = r.json()
         finished_on_backend = [x for x in response["objects"] if x["status"] == 1]
         return finished_on_backend
 
